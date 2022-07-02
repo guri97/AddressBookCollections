f75ade523c4da13af6acaa2b2e6693d8ae233aba
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-02 13:00:33 +0530

    [gurpreet] UC1_CreateContact

---
+++ AddressBookCollections/AddressBookCollections/AddressBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23_CollectionAddressBook
+{
+
+    internal class AddressBook
+    {
+
+        public Dictionary<string, Contact> contacts;
+
+        public AddressBook()
+        {
+            contacts = new Dictionary<string, Contact>();
+        }
+
+        public void CreateContact()
+        {
+            Contact tempContact = new Contact();
+            tempContact.GetUserInfo();
+            string name = tempContact.GetName();
+            if (contacts.ContainsKey(name) is false)
+            {
+                contacts.Add(name, tempContact);
+            }
+            else
+            {
+                Console.WriteLine("erorr");
+            }
+        }
+
+        public void Display()
+        {
+            foreach (string name in contacts.Keys)
+                contacts[name].Display();
+        }
+    }
+
+}
---
+++ AddressBookCollections/AddressBookCollections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day23_CollectionAddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book");
+            AddressBook address = new AddressBook();
+            address.CreateContact();
+            address.Display();
+            Console.ReadKey();
+        }
+    }
+}
