bbc0fc7c74e06328f329d3ab8a6213ca175f28b3
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-02 13:22:08 +0530

    [gurpreet] UC6_UniqueAddressBook

--- AddressBookCollections/AddressBookCollections/AddressBook.cs
+++ AddressBookCollections/AddressBookCollections/AddressBook.cs
@@ -111,4 +111,17 @@ namespace Day23_CollectionAddressBook
             }
         }
     }
+    internal class AdressBookSystem
+    {
+        public Dictionary<string, AddressBook> adressBooks = new Dictionary<string, AddressBook>();
+        public void AddAddressBook()
+        {
+            AddressBook adressBook = new AddressBook();
+            adressBook.AddMultiple();
+            Console.WriteLine("enter the addressbook name");
+            string addressName = Convert.ToString(Console.ReadLine());
+            adressBooks.Add(addressName.ToLower(), adressBook);
+
+        }
+    }
 }
