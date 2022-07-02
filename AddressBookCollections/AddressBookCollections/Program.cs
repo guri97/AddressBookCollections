61777ff4b846c3bb333ec6dca6604be7ced45a3f
guri97 <105732963+guri97@users.noreply.github.com>
2022-07-02 13:08:16 +0530

    [gurpreet] UC3_EditContact

--- AddressBookCollections/AddressBookCollections/Program.cs
+++ AddressBookCollections/AddressBookCollections/Program.cs
@@ -10,11 +10,49 @@ namespace Day23_CollectionAddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Address Book");
+            Console.WriteLine("===================Welcome To Address Book===================");
+            //Object Of Class
             AddressBook address = new AddressBook();
-            address.CreateContact();
-            address.Display();
-            Console.ReadKey();
+
+
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter Number to Execute the Address book Program \n1. Create contacts \n2. Add contact \n3. Edit contact \n4. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("Creating A New Contact");
+                        address.CreateContact();
+                        address.Display();
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Adding A New Contact");
+                        address.AddContacts();
+                        address.Display();
+                        Console.WriteLine();
+                        break;
+
+                    case 3:
+                        Console.WriteLine("Editing Existing Contact");
+                        address.EditContact();
+                        Console.WriteLine();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("If You Want To Exit Then Press Enter");
+                        flag = false;
+                        Console.ReadKey();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid");
+                        break;
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
